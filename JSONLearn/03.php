40121276e37c876eaba32904f72246a88a979740
admin <penghua.wang@condenast.com.cn>
2018-07-18 18:13:48 +0800

    无刷新方式使用附件上传

---
+++ JSONLearn/03.php
@@ -0,0 +1,25 @@
+<?php 
+
+//收集表单域信息$_POST 和 上传文件域信息 $_FILES
+// echo "post: ";
+
+// print_r($_POST);
+
+// echo "FILES: ";
+
+// print_r($_FILES);
+if ($_FILES['userpic']['error']>0) {
+	exit('附近有异常问题');
+}
+// 1.附件的存储的位置,附件的名字
+$path = "../upload/";
+$name = $_FILES['userpic']['name']; //原名字
+$truename = $path.$name;
+// echo $truename;
+// 2. 把附近从临时路径位置移动到真实位置
+if (move_uploaded_file($_FILES['userpic']['tmp_name'], $truename)) {
+ 	echo "上传成功";
+ }else{
+ 	echo "上传失败";
+ }
+ ?>
